316a5b1aa7dcfdc097b9f533dd5d40f9c3a4112a
Moisés <moisesnetored@gmail.com>
2025-05-31 23:14:07 -0400

    feat: adjust state and values

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,8 +6,18 @@ import { Label } from "./components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/ui/select";
 import { Slider } from "./components/ui/slider";
 import { VideoInputForm } from "./components/video-input-form";
+import { PromptSelect } from "./components/prompt-select";
+import { useState } from "react";
 
 export function App() {
+  const [temperature, setTemperature] = useState(0.5)
+  const [videoId, setVideoID] = useState<string | null>(null)
+
+
+  function handlePromptSelected(template: string) {
+    console.log(template)
+  }
+
   return (
     <div className="min-h-screen flex flex-col">
       <header className="px-6 py-3 flex items-center justify-between border-b">
@@ -45,7 +55,7 @@ export function App() {
         </div>
 
         <aside className="w-80 space-y-6">
-          <VideoInputForm />
+          <VideoInputForm onVideoUploaded={setVideoID} />
 
           <Separator />
 
@@ -53,16 +63,7 @@ export function App() {
             <div className="space-y-2">
               <Label>Prompt</Label>
 
-              <Select>
-                <SelectTrigger className="w-full">
-                  <SelectValue placeholder="Selecione um prompt..." />
-                </SelectTrigger>
-
-                <SelectContent>
-                  <SelectItem value="title">Título do YouTube</SelectItem>
-                  <SelectItem value="description">Descrição do YouTube</SelectItem>
-                </SelectContent>
-              </Select>
+              <PromptSelect onPromptSelected={handlePromptSelected} />
             </div>
     
 
@@ -93,7 +94,8 @@ export function App() {
                 min={0}
                 max={1}
                 step={0.1}
-                defaultValue={[0.5]}
+                value={[temperature]}
+                onValueChange={value => setTemperature(value[0])}
               />
 
               <span className="block text-xs text-muted-foreground italic">
